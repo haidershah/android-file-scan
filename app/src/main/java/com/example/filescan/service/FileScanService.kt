82578280b2f324933db43d302d4d48540a63e314
Rameez Shah <rshah@servicechannel.com>
2019-10-04 15:56:12 -0700

    Add status bar notification

--- app/src/main/java/com/example/filescan/service/FileScanService.kt
+++ app/src/main/java/com/example/filescan/service/FileScanService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.Environment
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.*
 import java.io.File
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
@@ -11,6 +12,10 @@ import com.example.filescan.receiver.FileScanReceiver.Companion.ACTION_SEND_COMP
 import com.example.filescan.receiver.FileScanReceiver.Companion.ACTION_SEND_FILE_NAME
 import com.example.filescan.receiver.FileScanReceiver.Companion.EXTRA_FILE_NAME
 import com.example.filescan.receiver.FileScanReceiver.Companion.EXTRA_IS_COMPLETED
+import android.app.NotificationManager
+import android.content.Context
+import com.example.filescan.R
+
 
 class FileScanService : Service() {
 
@@ -18,6 +23,7 @@ class FileScanService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         CoroutineScope(Dispatchers.IO + job).launch {
+            createNotification()
             scanFiles(Environment.getExternalStorageDirectory())
             sendCompletion()
         }
@@ -26,6 +32,23 @@ class FileScanService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    private fun createNotification() {
+        val channelId = "wow"
+        val notificationId = 100
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.database)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.scanning_files))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // notificationID allows you to update the notification later on.
+        notificationManager.notify(notificationId, builder.build())
     }
 
     private suspend fun scanFiles(file: File) {
